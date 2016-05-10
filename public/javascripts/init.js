0fbeeeeb7b2b9d87870c3fa18cf9fa7c2715d101
Luke Todd <lukejamestodd1@gmail.com>
2016-05-10 14:58:06 +1000

    hh

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -8,15 +8,10 @@
 })(jQuery); // end of jQuery name space
 
 $(function(){
-	if (window.location.href === '/'){
-		$('#homepage').addClass('current-page');
-	}
-	else{
-	  $('a').each(function() {
-	  	//if current url includes button link, change status
-			if (window.location.href.indexOf($(this).prop('href')) > -1) {
-		  	$(this).addClass('current-page');
-		  }
-	  });
-	}
+  $('a').each(function() {
+  	//if current url includes button link, change status
+		if (window.location.href.indexOf($(this).prop('href')) > -1) {
+	  	$(this).addClass('current-page');
+	  }
+  });
  });
