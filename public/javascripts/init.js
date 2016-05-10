0ee7d310a0c23935d9f7b22fe1aa8d5c7d61d00b
Luke Todd <lukejamestodd1@gmail.com>
2016-05-10 14:36:13 +1000

    current page highlight

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -8,10 +8,15 @@
 })(jQuery); // end of jQuery name space
 
 $(function(){
-  $('a').each(function() {
-  	//if current url includes button link, change status
-		if (window.location.href.indexOf($(this).prop('href')) > -1) {
-	  	$(this).addClass('current-page');
-	  }
-  });
+	if (window.location.href === '/'){
+		$('#homepage').addClass('current-page');
+	}
+	else{
+	  $('a').each(function() {
+	  	//if current url includes button link, change status
+			if (window.location.href.indexOf($(this).prop('href')) > -1) {
+		  	$(this).addClass('current-page');
+		  }
+	  });
+	}
  });
