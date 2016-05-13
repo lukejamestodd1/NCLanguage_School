a7d5acfc77c73678b80632bc0657bd08b53dbb05
aussieyang <yang.j.li91@gmail.com>
2016-05-13 14:52:07 +1000

    added layout highlighting in init.js

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -6,12 +6,28 @@
  }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+
+// $(function(){
+//   $('a').each(function() {
+//   	//if current url includes button link, change status
+//     if (window.location.href.indexOf($(this).prop('href')) > -1) {
+// 	  	$(this).addClass('current-page');
+//       $(this).addClass('active');
+// 	  }
+//   });
+//  });
+
 $(function(){
-  $('a').each(function() {
-  	//if current url includes button link, change status
-    if (window.location.href.indexOf($(this).prop('href')) > -1) {
-	  	$(this).addClass('current-page');
-      $(this).addClass('active');
-	  }
+  var tabs = ['enrol', 'campuses', 'news', 'blog', 'contact'];
+  $('.nav-wrapper a').each(function() {
+    for(var i = 0; i < tabs.length; i++) {
+      if (window.location.pathname.includes(tabs[i])) {
+        $('#homepage').removeClass('current-page');
+        $('#homepage').removeClass('active');
+        $('#'+tabs[i]).addClass('current-page');
+        $('#'+tabs[i]).addClass('active');
+      };
+    };
   });
  });
