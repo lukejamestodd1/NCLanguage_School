4d92a28e0ed1f13796e5d30ed164e8f0fb27bf79
aussieyang <yang.j.li91@gmail.com>
2016-07-13 16:17:38 +1000

    fixing images

--- app.js
+++ app.js
@@ -39,7 +39,10 @@ app.use(cookieParser());
 // }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+express.static.mime.define({'image/jpg': ['jpg']});
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use('/', routes);
