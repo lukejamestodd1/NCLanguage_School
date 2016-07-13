9ffc8a6bf590fe6a72895b4ddb4ae1bb2e00ec8e
aussieyang <yang.j.li91@gmail.com>
2016-07-13 11:02:41 +1000

    deploy

--- models/db.js
+++ models/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/newcentury');
+mongoose.connect('mongodb://heroku_213b1s47:ttultmf4li40esfjj83f6mprgv@ds055485.mlab.com:55485/heroku_213b1s47');
