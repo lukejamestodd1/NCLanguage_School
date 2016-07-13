e5cf69c327a456d7acc394bd919a903fbedec992
aussieyang <yang.j.li91@gmail.com>
2016-07-13 11:09:43 +1000

    deploy

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var articles = require('./models/articles');
 
 var app = express();
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 // view engine setup
 // app.set('views', './views');
 app.set('views', path.join(__dirname, 'views'));
