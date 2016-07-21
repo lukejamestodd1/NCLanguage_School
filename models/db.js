b0a2752262f5c3d3c1980b9a9802b04a28ab990d
aussieyang <yang.j.li91@gmail.com>
2016-07-21 15:40:57 +1000

    env file

--- models/db.js
+++ models/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_213b1s47:ttultmf4li40esfjj83f6mprgv@ds055485.mlab.com:55485/heroku_213b1s47');
+mongoose.connect(process.env.MONGODB_URI);
