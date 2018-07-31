d41c71c00a4421fda79d6845cb0718beef73ad42
Luke Todd <lukejamestodd1@gmail.com>
2018-07-31 14:56:55 +1000

    packages

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var articles = require('./models/articles');
 
 var app = express();
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 // view engine setup
